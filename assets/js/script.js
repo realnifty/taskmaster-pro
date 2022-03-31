e1bb0a71c4ae129b6e88a6f366336abbb12fddb6
gordo <rogelioalbertopedraza@gmail.com>
2022-03-31 18:48:34 -0500

    5.3.6 leave-off

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,6 +129,48 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = {};
+
+    $(this).children().each(function() {
+      var text = $(this).find("p").text().trim();
+
+      var date = $(this).find("p").text().trim();
+
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    console.log(tempArr)
+  }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
